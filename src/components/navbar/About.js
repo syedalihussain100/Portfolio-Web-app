4878db214d2206f1219e08edf6d07c4495bfbdcd
syedalihussain100 <smuheminali@gmail.com>
2021-10-07 21:36:37 +0500

    some chnages

--- src/components/navbar/About.js
+++ src/components/navbar/About.js
@@ -4,6 +4,10 @@ function About() {
     return (
         <div>
             <h1>Hello About.js</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Magni soluta cupiditate vitae id minima aspernatur,
+                laudantium incidunt harum nostrum quos placeat ab sapiente repudiandae
+                libero dolorem omnis tempore, non iure!</p>
         </div>
     )
 }
--- src/components/navbar/Home.js
+++ src/components/navbar/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Typical from 'react-typical'
+
 
 function Home() {
     return (
         <div>
-       <h1>This is H1 components</h1>      
+            <h1>This is H1 components</h1>
+            <p style={{ textAlign: 'center', fontSize: '30px', color: 'red' }}> <span style={{ color: 'black' }}>I'm a </span>
+                <Typical
+                    loop={Infinity}
+                    wrapper="b"
+                    steps={[
+                        'Front End Developer',
+                        1000,
+                        'Back End Developer',
+                        1000,
+                        'Full Stack Developer',
+                        1000,
+                        'Senior Software Enginerring',
+                        1000,
+                        'Professional Software Enginerring',
+                        1000
+                    ]}
+                />
+            </p>
         </div>
     )
 }
