1a0b05de024b89d301a08f611ee9ca5246e79724
syedalihussain100 <smuheminali@gmail.com>
2021-10-08 10:39:10 +0500

    create components folder

---
+++ src/components/about/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+       <p>About pages here</p>            
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <p>Home pages here</p>
+        </div>
+    )
+}
+
+export default Home
